ebd01a7fe6cc66ff1635f1bd286689e6abad0b81
DrNykterstein <drnykterstein@MacBook-Air.local>
2025-07-20 12:11:42 +0530

    fixed path issues

--- src/app/api/contact/route.jsx
+++ src/app/api/contact/route.jsx
@@ -1,5 +1,5 @@
-import connectDB from '@/libs/connectDB';
-import Contact from '@/libs/contact_Schema'
+import connectDB from '../../../libs/connectDB';
+import Contact from '../../../libs/contact_Schema'
 import { NextResponse } from 'next/server'
 
 export async function POST(req) {
