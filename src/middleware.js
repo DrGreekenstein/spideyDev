6d8966b4f7e763f38e3c6d0979a7f605acf787da
DrNykterstein <drnykterstein@MacBook-Air.local>
2025-07-20 15:46:39 +0530

    trying to fix payment 6

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export default authMiddleware({
+export default clerkMiddleware({
   async afterAuth(auth, req) {
     const plan = auth?.user?.publicMetadata?.plan || 'basic';
     const url = req.nextUrl;
