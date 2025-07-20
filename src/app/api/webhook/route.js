cece2c5c43e391217fd5fbacd6cb9b240a08739e
DrNykterstein <drnykterstein@MacBook-Air.local>
2025-07-20 15:01:13 +0530

    trying to fix payment 2

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-import connectDB from "../../../libs/connectDB";
-import User from "../../../libs/user_Schema";
+import connectDB from "../../../libs/connectDB.js";
+import User from "../../../libs/user_Schema.js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2025-06-30.basil",
